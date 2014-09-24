2f68734e9903bc2bd755174261a5f45bf50875c4
Florie <floriecai@hotmail.com>
2014-09-24 02:23:44 -0700

    adding Stockwatcher project

---
+++ src/com/google/gwt/sample/stockwatcher/client/NotLoggedInException.java
@@ -0,0 +1,12 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+public class NotLoggedInException extends Exception {
+
+	public NotLoggedInException() {
+		super();
+	}
+
+	public NotLoggedInException(String message) {
+		super(message);
+	}
+}
